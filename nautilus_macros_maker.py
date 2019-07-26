5391af005b5d394f282736cf203c8b513f9a40c2
Zach <zachrose@hydraresearch3d.com>
2019-07-26 11:30:15 -0700

    added versioning to python scripts

--- nautilus_macros_maker.py
+++ nautilus_macros_maker.py
@@ -12,15 +12,28 @@ def fileList(fileName):
         files += [os.path.join(dirpath, file) for file in filenames]
     return files
 
+print("What version is this config? ")
+versNo = str(input())
 path = os.path.dirname(os.path.realpath(__file__))
 mac = 'macros'
 
 with tempfile.TemporaryDirectory() as macfolder:
     copy_tree(os.path.join(path,mac),macfolder)
-    with zipfile.ZipFile(os.path.join(path,'Nautilus_Macros_1.0.zip'),'w') as maczip:
+    with zipfile.ZipFile(os.path.join(path,'Nautilus_Macros_'+versNo+'.zip'),'w') as maczip:
         contents = fileList(macfolder)
         for item in contents:
-            if os.path.basename(item)!='desktop.ini':
+            if 'Icon' in item:
+                contents.remove(item)
+                continue
+            if item!=os.path.join(macfolder, 'desktop.ini'):
+                f = open(item,'r+')
+                lines = f.readlines()
+                f.seek(0)
+                f.write('; Configuration for the Nautilus 3D printer by Hydra Research \n')
+                f.write('; Version: '+versNo+' \n')
+                for line in lines:
+                    f.write(line)
+                f.close()
                 #print(item)
                 maczip.write(os.path.join(macfolder, item), os.path.relpath(item, macfolder))
     maczip.close()
--- nautilusconfigmaker.py
+++ nautilusconfigmaker.py
@@ -5,15 +5,22 @@ import shutil
 import zipfile
 import tempfile
 from distutils.dir_util import copy_tree
+import json
+import requests
+import urllib
 
 def fileList(fileName):
     files = list()
     for (dirpath, dirnames, filenames) in os.walk(fileName):
         files += [os.path.join(dirpath, file) for file in filenames]
     return files
-
+print("What version is this config? ")
+versNo = str(input())
 path = os.path.dirname(os.path.realpath(__file__))
-dwc = 'DuetWebControl-Duet2.zip'
+zip_file_url = json.loads(requests.get('https://api.github.com/repos/dc42/reprapfirmware/releases/latest').text)['assets'][1]['browser_download_url']
+urllib.request.urlretrieve(zip_file_url,os.path.join(path,os.path.basename(zip_file_url)))
+
+dwc = os.path.basename(zip_file_url)
 duet = 'Duet2Firmware-2.03.zip'
 sys = 'sys'
 
@@ -23,9 +30,22 @@ with tempfile.TemporaryDirectory() as configfolder:
         dwczip.extractall(configfolder)
     with zipfile.ZipFile(os.path.join(path, duet)) as duetzip:
         duetzip.extractall(configfolder)
-    with zipfile.ZipFile(os.path.join(path,'Nautilus_Configuration_1.0.zip'),'w') as configzip:
+    with zipfile.ZipFile(os.path.join(path,'Nautilus_Test_Configuration_'+versNo+'.zip'),'w') as configzip:
         contents = fileList(configfolder)
         for item in contents:
+            if 'Icon' in item:
+                contents.remove(item)
+                continue
+            print(os.path.basename(item))
             if item!=os.path.join(configfolder, 'desktop.ini'):
+                if item.endswith('.g') or item.endswith('.bak'):
+                    f = open(item,'r+')
+                    lines = f.readlines()
+                    f.seek(0)
+                    f.write('; Configuration for the Nautilus 3D printer by Hydra Research \n')
+                    f.write('; Version: '+versNo+' \n')
+                    for line in lines:
+                        f.write(line)
+                    f.close()
                 configzip.write(os.path.join(configfolder, item), os.path.relpath(item, configfolder))
     configzip.close()
